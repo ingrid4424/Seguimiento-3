0576f13733be40d8ea1961a7bac1ca238a23485c
Ingrid Vallecilla <stepha4424@gmail.com>
2021-05-18 21:26:53 -0500

    creación figura abstract

---
+++ src/Figura.java
@@ -0,0 +1,88 @@
+import modelo.Color;
+import processing.core.PApplet;
+
+public abstract class Figura {
+
+	protected int posX;
+	protected int posY;
+	protected int tamX;
+	protected int tamY;
+	protected PApplet app;
+	protected Color colors;
+	
+	public Figura(int posX, int posY, int tamX, int tamY, PApplet app) {
+		this.posX = posX;
+		this.posY = posY;
+		this.tamX = tamX;
+		this.tamY = tamY;
+		this.app = app;
+		this.colors = new Color();
+	}
+	
+	public  abstract void pintar();
+	
+	
+
+	public void pintarColor() {
+		try {
+			this.app.fill(colors.getR(),colors.getG(),colors.getB());
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public int getTamX() {
+		return tamX;
+	}
+
+	public void setTamX(int tamX) {
+		this.tamX = tamX;
+	}
+
+	public int getTamY() {
+		return tamY;
+	}
+
+	public void setTamY(int tamY) {
+		this.tamY = tamY;
+	}
+
+	public PApplet getApp() {
+		return app;
+	}
+
+	public void setApp(PApplet app) {
+		this.app = app;
+	}
+	
+	public Color getColors() {
+		return colors;
+	}
+
+	public void setColors(Color colors) {
+		this.colors = colors;
+	}
+	
+	
+	
+	
+}
---
+++ src/modelo/Color.java
@@ -0,0 +1,52 @@
+package modelo;
+
+public class Color{
+	
+	private int r;
+	private int g;
+	private int b;
+	private boolean isColor;
+	
+	public Color() {
+		this.isColor = false;
+	}
+	
+	public void limpiarColor() {
+		this.r = 0;
+		this.g = 0;
+		this.b = 0;
+		this.isColor = false;
+	}
+	
+	public String getFinalColor() {
+		String rgb = r + g + b +"";
+		return rgb;
+	}
+
+	public int getR() {
+		return r;
+	}
+
+	public void setR(int r) {
+		this.r = r;
+	}
+
+	public int getG() {
+		return g;
+	}
+
+	public void setG(int g) {
+		this.g = g;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+	
+	
+
+}
