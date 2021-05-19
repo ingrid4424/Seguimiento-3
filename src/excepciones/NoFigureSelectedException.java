a70e6a930e6c9286fc95c7a5261fb317d8cda001
Ingrid Vallecilla <stepha4424@gmail.com>
2021-05-18 20:00:27 -0500

    Creacion de excepciones

---
+++ src/excepciones/NoFigureSelectedException.java
@@ -0,0 +1,16 @@
+package excepciones;
+
+public class NoFigureSelectedException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoFigureSelectedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
---
+++ src/excepciones/NoSameColorException.java
@@ -0,0 +1,21 @@
+package excepciones;
+
+public class NoSameColorException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+
+	public NoSameColorException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+
+}
