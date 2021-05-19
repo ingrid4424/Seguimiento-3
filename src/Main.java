b6ee40109e008f721a51ac98e23a6ba78cfe4fa5
Ingrid Vallecilla <stepha4424@gmail.com>
2021-05-18 19:44:44 -0500

    base de Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
+import processing.core.PApplet;
 
-public class Main {
+public class Main extends PApplet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PApplet.main(Main.class.getName());
+	}
+	
+	public void settings() {
+		size(1200,700);
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void mousePressed() {
+		
 	}
 
 }
